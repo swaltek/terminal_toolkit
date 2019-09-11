3d5f297d9e29200fc3fc31f49ed53eafd9c2ff94
Marcin <marcinswaltek@gmail.com>
2019-09-11 19:39:25 -0400

    Fixed CTK_Tileset

--- lib/CTK_Tileset.cpp
+++ lib/CTK_Tileset.cpp
@@ -1,4 +1,4 @@
-#include "CTK_Console.h"
+#include "CTK_Tileset.h"
 
 #include "SDL2/SDL.h"
 #include <cstdio>
@@ -6,17 +6,21 @@
 const unsigned TILESET_WIDTH = 16;
 const unsigned TILESET_HEIGHT = 16;
 
-CTK_Tileset::CTK_Tileset(const char* file_path)
+CTK_Tileset::CTK_Tileset(SDL_Renderer* renderer): tile_renderer(renderer)
+{
+}
+CTK_Tileset::CTK_Tileset(SDL_Renderer* renderer, const char* file_path): tile_renderer(renderer)
 {
 	load_from_bmp( file_path );
 }
 
 CTK_Tileset::~CTK_Tileset()
 {
+	tile_renderer = nullptr;
 	free();
 }
 
-CTK_Tileset::load_from_bmp(const char* file_path)
+bool CTK_Tileset::load_from_bmp(const char* file_path)
 {
 	free(); //Get rid of any already existing texture
 
@@ -30,15 +34,16 @@ CTK_Tileset::load_from_bmp(const char* file_path)
 	else
 	{
 		//Color key image to first pixel
-		SDL_LockTexture( loaded_surface ); //Lock surface before messing with pixels
-		void * first_pixel = loaded_surface->pixels[0];
+		SDL_LockSurface( loaded_surface ); //Lock surface before messing with pixels
+		unsigned first_pixel = static_cast<unsigned*>(loaded_surface->pixels)[0];
 		SDL_UnlockSurface( loaded_surface ); //Unlock surface before changing color key
+
 		if( SDL_SetColorKey( loaded_surface, SDL_TRUE, first_pixel ) )
 		{
 			printf( "Error color keying! SDL Error %s\n", SDL_GetError() );
 		}
 
-		new_texture = SDL_CreateTextureFromSurface( CTK_Renderer, loaded_surface );
+		new_texture = SDL_CreateTextureFromSurface( tile_renderer, loaded_surface );
 		if( new_texture == NULL )
 		{
 			printf( "Unable to create texture from %s! SDL Error: %s\n", file_path, SDL_GetError() );
@@ -54,8 +59,8 @@ CTK_Tileset::load_from_bmp(const char* file_path)
 			tile_clips = new SDL_Rect[ tile_clips_size ];
 			for(unsigned i{ 0 }; i < tile_clips_size ; ++i )
 			{
-				tile_clips[i].x = i * tile_width;
-				tile_clips[i].y = (i % TILESET_WIDTH) * tile_height;
+				tile_clips[i].x = (i % TILESET_WIDTH) * tile_width;
+				tile_clips[i].y = (i / TILESET_WIDTH) * tile_height;
 				tile_clips[i].w = tile_width;
 				tile_clips[i].h = tile_height;
 			}
@@ -70,27 +75,28 @@ CTK_Tileset::load_from_bmp(const char* file_path)
 	return texture != NULL;
 }
 
-CTK_Tileset::render(int x, int y, char clip)
+void CTK_Tileset::render(int x, int y, char clip)
 {
 	SDL_Rect render_clip = { 0, 0, width, height };
 	//if not rendering the entire image
-	if( clip != '\n' ) render_clip = &tile_clips[clip];
+	if( clip != '\n' ) render_clip = tile_clips[clip];
 
-	SDL_Rect render_rect = { x, y, render_clip.w, render_clip.h };
-	SDL_RenderCopy( CTK_Renderer, texture, render_clip, &render_rect);
+	SDL_Rect render_rect = { x * tile_width , y * tile_height, render_clip.w, render_clip.h };
+
+	SDL_RenderCopy( tile_renderer, texture, &render_clip, &render_rect);
 }
 
-CTK_Tileset::free()
+void CTK_Tileset::free()
 {
 	//Free existing texture if it exists
-	if( texture !- NULL )
+	if( texture != NULL )
 	{
 		SDL_DestroyTexture( texture );
 		texture = NULL;
 
 		delete[] tile_clips;
-		tile_clips = NULL;
-		tile_clips_size = NULL;
+		tile_clips = nullptr;
+		tile_clips_size = 0;
 
 		width = 0;
 		height = 0;
--- lib/CTK_Tileset.h
+++ lib/CTK_Tileset.h
@@ -6,11 +6,11 @@
 class CTK_Tileset
 {
 	public:
-		CTK_Tileset();
-		CTK_Tileset(char*);
+		CTK_Tileset(SDL_Renderer*);
+		CTK_Tileset(SDL_Renderer*, const char*);
 		~CTK_Tileset();
 
-		bool load_from_file(const char*);
+		bool load_from_bmp(const char*);
 
 		void render(int, int, char = '\n');
 
@@ -18,12 +18,14 @@ class CTK_Tileset
 	private:
 		SDL_Texture* texture { NULL };
 
+		SDL_Renderer* tile_renderer;
+
 		SDL_Rect* tile_clips { NULL };
 		unsigned tile_clips_size { 0 };
 
-		unsigned width { 0 };
-		unsigned height { 0 };
-		unsigned tile_width { 0 };
-		unsigned tile_height { 0 };
-}
+		int width { 0 };
+		int height { 0 };
+		int tile_width { 0 };
+		int tile_height { 0 };
+};
 #endif
